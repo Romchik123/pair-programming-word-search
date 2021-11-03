c9fb1b80ee0218ece63768385084ae4d14670033
rom <mozzieisme@gmail.com>
2021-11-03 16:42:37 -0400

    Create new files

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const varticalJoint = function(letters, word) {
+  let newLetters = [];
+  for (let row = 0; row < letters[0].length; row++) {
+    let newRow = [];
+    for (let col = 0; col < letters.length; col++) {
+      newRow.push(letters[col][row]);
+    }
+    newLetters.push(newRow);
+  }
+
+  const varticalArray = newLetters.map((ls) => ls.join(""));
+  for (let l of varticalArray) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = { wordSearch, varticalJoint };
